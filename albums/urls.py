0ffe85cdbe6735f2c36c6eca547c14fa4417f2ba
Elaiza0203 <baldonadoelaiza03@gmail.com>
2026-05-23 17:25:35 +0800

    Fixed album detail and models

--- albums/urls.py
+++ albums/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import (
     AlbumListView,
     AlbumDetailView,
@@ -9,10 +10,39 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', AlbumListView.as_view(), name='album-list'),
-    path('album/<int:pk>/', AlbumDetailView.as_view(), name='album-detail'),
-    path('album/create/', AlbumCreateView.as_view(), name='album-create'),
-    path('album/<int:pk>/update/', AlbumUpdateView.as_view(), name='album-update'),
-    path('album/<int:pk>/delete/', AlbumDeleteView.as_view(), name='album-delete'),
-    path('photo/create/', PhotoCreateView.as_view(), name='photo-create'),
+    path(
+        '',
+        AlbumListView.as_view(),
+        name='album-list'
+    ),
+
+    path(
+        'album/<int:pk>/',
+        AlbumDetailView.as_view(),
+        name='album-detail'
+    ),
+
+    path(
+        'album/create/',
+        AlbumCreateView.as_view(),
+        name='album-create'
+    ),
+
+    path(
+        'album/<int:pk>/update/',
+        AlbumUpdateView.as_view(),
+        name='album-update'
+    ),
+
+    path(
+        'album/<int:pk>/delete/',
+        AlbumDeleteView.as_view(),
+        name='album-delete'
+    ),
+
+    path(
+        'album/<int:pk>/upload/',
+        PhotoCreateView.as_view(),
+        name='photo-upload'
+    ),
 ]
